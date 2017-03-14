d93072b37102f259ccd9246b40224712bd8be174
Anthony Santiago <asantiago323@gmail.com>
2017-03-13 20:43:04 -0500

    imported 'HeroDetailComponent' to module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,17 @@ import { HeroService } from './hero.service';
   styleUrls: ['./app.component.css'],
   providers: [HeroService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = Hero[];
+  heroes: Hero[];
   selectedHero: Hero;
-
-  constructor(private heroService: HeroService){ }
-
+  constructor(private heroService: HeroService) { }
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
-  
   ngOnInit(): void {
     this.getHeroes();
   }
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-
 @Injectable()
 export class HeroService {
-    //fetches the heroes object (list of heroes)
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
-    }
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 }
